053436189aa4180244e3488aaaa87edb3c6e087d
dopoku12 <dopoku.tech@gmail.com>
2025-02-14 17:33:38 -0500

    server added

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -6,20 +6,23 @@ $(document).ready(function () {
     
     $(".card-body").each(function() {
        
-      let count=0
+      let count=1
       const $btn=$(this).find('.btn')
       const $cardTitle=$(this).find('.card-title')
 
       $btn.on("click", function () {
-            count++
-            console.log(count)
-            localStorage.setItem('cartCount',`${num++}`)
-            console.log($cardTitle.text());
+        sessionStorage.setItem('cartCount',`${num++}`)
+        
+        if(!sessionStorage.getItem($cardTitle.text())){
 
-            localStorage.setItem($cardTitle.text(),$cardTitle.text())
-            
-
-            badge.text(localStorage.cartCount)
+          console.log('nothing')
+          sessionStorage.setItem($cardTitle.text(),$cardTitle.text())
+          sessionStorage.setItem(`${$cardTitle.text()} Count`,`${count++}`)
+        }
+        else {
+          sessionStorage.setItem(`${$cardTitle.text()} Count`,`${count++}`)
+        }
+          badge.text(sessionStorage.cartCount)
         });
       
 
