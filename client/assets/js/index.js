fdc3c8deb426b54c64cd60d420506bacb2291266
dopoku12 <dopoku.tech@gmail.com>
2025-02-16 01:21:28 -0500

    local storage added

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,30 +1,55 @@
 "strict"
+
 $(document).ready(function () {
   let num=1
-
-    const badge=$('.badge')
+  const badge=$('.badge')
+  
     
-    $(".card-body").each(function() {
+  $(".card-body").each(function() {
        
-      let count=1
-      const $btn=$(this).find('.btn')
-      const $cardTitle=$(this).find('.card-title')
+    let count=1
+    const $btn=$(this).find('.btn')
+    const $cardTitle=$(this).find('.card-title')
+    const $cardText=$(this).find(".card-text")
 
-      $btn.on("click", function () {
+    $btn.on("click", function () {
         sessionStorage.setItem('cartCount',`${num++}`)
         
         if(!sessionStorage.getItem($cardTitle.text())){
-
-          console.log('nothing')
           sessionStorage.setItem($cardTitle.text(),$cardTitle.text())
+          sessionStorage.setItem(`${$cardTitle.text()} Price`,$cardText.text())
           sessionStorage.setItem(`${$cardTitle.text()} Count`,`${count++}`)
         }
         else {
           sessionStorage.setItem(`${$cardTitle.text()} Count`,`${count++}`)
         }
-          badge.text(sessionStorage.cartCount)
-        });
+          
+    });
       
 
-      });
+  });
+
+  
+
+
+  $("#contact-form").submit(function (e) { 
+    e.preventDefault();
+    
+    const $contactEmail=$('#contact-email')
+    const $contactMsg=$('#contact-message')
+    localStorage.setItem('contact-email',$contactEmail.val())
+    localStorage.setItem('msg',$contactMsg.val())
+    $(this).trigger("reset")
+  }) 
+
+
+
+  $('#email-form').keydown(function(e){
+    const $value=$('#FormControlInput1').val();
+    if(e.key==="Enter")
+    localStorage.setItem('email',$value)
+    
+  })
+
+  
 });
